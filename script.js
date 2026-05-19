8564647884339a92f267a4baf2d4129801f738ce
zghammett <zghammett@gmail.com>
2026-05-18 20:33:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("Red White & Clean website loaded");
+
+document.querySelectorAll('.carousel').forEach(carousel => {
+  const track = carousel.querySelector('.carousel-track');
+  const slides = track.querySelectorAll('img');
+  const next = carousel.querySelector('.next');
+  const prev = carousel.querySelector('.prev');
+
+  let index = 0;
+
+  function update(){
+    track.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  next.addEventListener('click', () => {
+    index = (index + 1) % slides.length;
+    update();
+  });
+
+  prev.addEventListener('click', () => {
+    index = (index - 1 + slides.length) % slides.length;
+    update();
+  });
+
+  // OPTIONAL AUTO SLIDE (uncomment if you want it)
+  /*
+  setInterval(() => {
+    index = (index + 1) % slides.length;
+    update();
+  }, 4000);
+  */
+});
